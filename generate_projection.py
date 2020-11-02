a0d6e6ccb441a99ffe55cd7763352625fd62a2a4
Auguste <auguste@MacBook-Pro.lan>
2020-11-02 09:01:27 +0800

    add lmnp fiscality logic begin

--- generate_projection.py
+++ generate_projection.py
@@ -2,15 +2,16 @@ import pandas as pd
 import itertools
 import numpy as np
 from tqdm import tqdm
+import joblib
 
 
 APPORTS_TAUX = [0.1]
-TAUX_OCCUPATION = np.arange(0.7, 0.9, 0.05)
-ECART_A_LA_MOYENNE_LOYER = np.arange(-0.1, 0.7, 0.05)
-ECART_A_LA_MOYENNE_CHARGES_COPRO = np.arange(-0.3, 0.3, 0.05)
-ECART_A_LA_MOYENNE_PRIX_M2_AVANT_TRAVAUX = np.arange(-0.5, 0.3, 0.05)
+TAUX_OCCUPATION = np.arange(0.7, 0.9, 0.1)
+ECART_A_LA_MOYENNE_LOYER = np.arange(-0.1, 0.7, 0.1)
+ECART_A_LA_MOYENNE_CHARGES_COPRO = np.arange(-0.3, 0.3, 0.1)
+ECART_A_LA_MOYENNE_PRIX_M2_AVANT_TRAVAUX = np.arange(-0.5, 0.3, 0.1)
 ANNEES_CREDIT_LIST = [20]
-TAUX_INTERET = np.arange(0.015, 0.02, 0.001)
+TAUX_INTERET = np.arange(0.015, 0.02, 0.005)
 OFF_MARKET = [True, False]
 VARIABLES_TO_COMBINE = [APPORTS_TAUX, TAUX_OCCUPATION, ECART_A_LA_MOYENNE_LOYER, ANNEES_CREDIT_LIST,
                         TAUX_INTERET, ECART_A_LA_MOYENNE_PRIX_M2_AVANT_TRAVAUX, ECART_A_LA_MOYENNE_CHARGES_COPRO,
@@ -18,7 +19,7 @@ VARIABLES_TO_COMBINE = [APPORTS_TAUX, TAUX_OCCUPATION, ECART_A_LA_MOYENNE_LOYER,
 
 # source taxe fonciere: https://www.tacotax.fr/guides/impots-locaux/taxe-fonciere/methode-de-calcul
 TAXE_FONCIERE_TAUX = {'reims':0.2926, 'saint_denis':0.2265, 'paris':0.0837, 'marseille': 0.2402,
-                      'strasbourg': 0.2249, 'rennes':0.2576}
+                      'strasbourg': 0.2249, 'rennes':0.2576, 'tinqueux': 0.155}
 
 COUT_MEUBLES = 7000
 COUT_TRAVAUX = 11000
@@ -40,7 +41,7 @@ ASSURANCE_GLI_ANNUEL_TAUX = 0.035 # assurance garantie loyes impayes (https://re
 ASSURANCE_EMPRUNTEUR_ANNUEL = 15*12
 
 # Pour des ordres de grandeurs pour les amortissements: https://gerant-immo.com/guide/LMNP-amortissement
-ANNEES_AMORTISSEMENT_BIEN = 20
+TAUX_AMORTISSEMENT_BIEN = 0.03
 ANNEES_AMORTISSEMENT_MOBILIER = 5
 ANNEES_AMORTISSEMENT_TRAVAUX = 10
 AMORTISSEMENT_MOBILIER_ANNUEL = COUT_MEUBLES/ANNEES_AMORTISSEMENT_MOBILIER
@@ -66,7 +67,8 @@ def get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone=None, lo
                       'loyer_annuel_percu': [], 'charges_annuel': [], 'taux_occupation': [],
                       'apport_total': [],
                       'ecart_prix_m2_prix_moyen_avant': [], 'ecart_prix_m2_prix_moyen_apres': [],
-                      'prix_m2_avant': [], 'prix_m2_apres': [], 'ecart_aux_charges_copro_moyen': []}
+                      'prix_m2_avant': [], 'prix_m2_apres': [], 'ecart_aux_charges_copro_moyen': [],
+                      'first_year_imposition': []}
 
 
 
@@ -86,8 +88,8 @@ def get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone=None, lo
         cout_bien = prix_m2_avant * superficie_bien_m2
 
         frais_notaire = cout_bien * FRAIS_NOTAIRE_TAUX
-        amortissement_bien_annuel = cout_bien / ANNEES_AMORTISSEMENT_BIEN
-        amortissements = amortissement_bien_annuel + AMORTISSEMENT_MOBILIER_ANNUEL + AMORTISSEMENT_TRAVAUX_ANNUEL
+        amortissement_bien_annuel = TAUX_AMORTISSEMENT_BIEN * cout_bien
+        amortissements_hors_bien = AMORTISSEMENT_MOBILIER_ANNUEL + AMORTISSEMENT_TRAVAUX_ANNUEL
 
         montant_empruntable = cout_bien + COUT_TRAVAUX + COUT_PLAN
         cout_sans_notaire = montant_empruntable + COUT_MEUBLES
@@ -136,15 +138,11 @@ def get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone=None, lo
         charges_annuel_avant_remboursement_credit = depenses_annuelles_gestion_bien + taxe_fonciere
         charges_annuel = charges_annuel_avant_remboursement_credit + remboursement_annuel_credit
 
-        # Calcul des charges annuelles pour la fiscalite LMNP
-        charges_annuel_LMNP = charges_annuel_avant_remboursement_credit + amortissements
-
-        # La premiere annee, on peut ajouter les frais de notaire aux charges
-        # TODO confirmer que l'on puisse aussi ajouter la totalite des interets aux charges la premiere annee
-        charges_annuel_LMNP_premiere_annee = charges_annuel_LMNP + frais_notaire + interet
-
-        montant_imposable_LMNP = loyer_annuel_percu - charges_annuel_LMNP
-        montant_imposable_LMNP_premiere_annee = loyer_annuel_percu - charges_annuel_LMNP_premiere_annee
+        first_year_of_imposition, compta_par_an = get_fiscalite_lmnp_par_an(annees_credit, loyer_annuel_percu,
+                                                                            amortissement_bien_annuel,
+                                                                            amortissements_hors_bien,
+                                                                            charges_annuel_avant_remboursement_credit,
+                                                                            frais_notaire, interet)
 
         # Pour les couts annuels, on ajoute les frais de notaire et l'apport ammortis sur la duree
         # du pret
@@ -152,9 +150,9 @@ def get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone=None, lo
 
         # On ne s'interesse qu'a la premiere annee. Pour simplifier, on fait l'hypothese que
         # la valeur de la variable doit_payer_impot restera la meme pour de nombreuses annees ensuite
-        if montant_imposable_LMNP_premiere_annee > 0:
+        if first_year_of_imposition == 1:
             doit_payer_impot = True
-            #TODO generer la projection dans le cas ou l'impot est du
+            #TODO generer la projection dans le cas ou l'impot est dû
         else:
             cash_flow = loyer_annuel_percu - charges_annuel
             cash_flow_total = loyer_annuel_percu - couts_annuel_total
@@ -179,19 +177,103 @@ def get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone=None, lo
             projection_dic['prix_m2_avant'].append(prix_m2_avant)
             projection_dic['prix_m2_apres'].append(prix_m2_apres)
             projection_dic['ecart_aux_charges_copro_moyen'].append(ecart_aux_charges_copro_moyen)
+            projection_dic['first_year_imposition'].append(first_year_of_imposition)
 
     projection_df = pd.DataFrame(projection_dic)
 
-    return projection_df
+    return projection_df, compta_par_an
+
+def get_fiscalite_lmnp_par_an(annees_credit, loyer_annuel_percu, amortissement_bien_annuel, amortissements_hors_bien,
+                              charges_annuel_avant_remboursement_credit, frais_notaire, interet):
+    """
+    Sources:
+    https://www.smartloc.fr/blog/fiscalite-de-location-meublee-regime-reel-en-lmnp/
+
+    https://www.legifrance.gouv.fr/codes/id/LEGIARTI000006302900/2008-05-01/
+
+    Quelques regles:
+    * L'amortissement du bien ne peut creer un deficit ou l'augmenter si il existe deja suite aux imputations des
+        depenses reelles (charges directes ou travaux amortis)
+    * Il est possible de creer un deficit a partir des seules depenses reelles
+    * Un deficit sur une annee (qui ne peut etre cree que par des depenses reelles) peut etre reporte sur les exercices
+        suivants dans la limite de 10 ans
+    * La part non utilisee de l'amortissement du bien peut etre reporte indefiniment
+
+    Args:
+        annees_credit:
+        loyer_annuel_percu:
+        amortissement_bien_annuel:
+        amortissements_hors_bien:
+        charges_annuel_avant_remboursement_credit:
+        frais_notaire:
+        interet:
+
+
+    Returns:
+
+    """
+    compta_par_an = {}
+
+    first_year_of_imposition = 0
+
+    report = 0
+    deficit_memory = {'deficit_restant_amort_bien': 0}
+
+    for i in range(1, annees_credit+1):
+        compta_annee_i = {}
+
+        compta_annee_i['loyers'] = loyer_annuel_percu
+
+        # La premiere annee, on peut ajouter les frais de notaire aux charges
+        # TODO confirmer que l'on puisse aussi ajouter la totalite des interets aux charges la premiere annee
+        if i == 1:
+            charges_avant_amort_bien =  (amortissements_hors_bien + charges_annuel_avant_remboursement_credit
+                                         + frais_notaire + interet)
+        else:
+            charges_avant_amort_bien = (amortissements_hors_bien + charges_annuel_avant_remboursement_credit
+                                        + report)
+            report = 0
+
+        charges_apres_amort_bien = charges_avant_amort_bien + amortissement_bien_annuel
+
+        revenus_imposables_avant_amort_bien = loyer_annuel_percu - charges_avant_amort_bien
+
+        revenus_imposables_apres_amort_bien = loyer_annuel_percu - charges_apres_amort_bien
+
+        # si il existe deja un deficit avant la prise en compte de l'amortissement du bien, ou si la prise en compte
+        # de l'amortissement du bien engendre un deficit, alors on reporte l'amortissement du bien a l'annee d'apres
+        if revenus_imposables_avant_amort_bien < 0 or revenus_imposables_apres_amort_bien < 0:
+            deficit_depenses_reelles = np.abs(revenus_imposables_avant_amort_bien)
+            report += amortissement_bien_annuel + deficit_avant_amort
+            compta_annee_i['charges_totales'] = charges_avant_amort_bien
+            compta_annee_i['revenus_imposables'] = revenus_imposables_avant_amort_bien
+            compta_annee_i['amort_bien_reporte'] = True
+            compta_annee_i['imposable'] = False
+            compta_annee_i['report'] = report
+            deficit_memory[i] = {'deficit_restant_depenses_reelles': deficit_depenses_reelles,
+                                 'annees_restants': 10}
+            deficit_memory['deficit_restant_amort_bien'] += amortissement_bien_annuel
+        else:
+            if first_year_of_imposition == 0:
+                first_year_of_imposition = i
+            compta_annee_i['charges_totales'] = charges_apres_amort_bien
+            compta_annee_i['revenus_imposables'] = revenus_imposables_apres_amort_bien
+            compta_annee_i['amort_bien_reporte'] = False
+            compta_annee_i['imposable'] = True
+            compta_annee_i['report'] = report
+
+        compta_par_an[i] = compta_annee_i
+
+    return first_year_of_imposition, compta_par_an
 
 
 if __name__=='__main__':
     ville = 'reims'
     superficie_bien_m2 = 50
-    prix_m2_moyen_zone = 2073
+    prix_m2_moyen_zone = 2300
     loyer_m2_moyen_zone = 9.8
 
-    projection_df = get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone, loyer_m2_moyen_zone)
+    projection_df, compta_par_an = get_projection_report(ville, superficie_bien_m2, prix_m2_moyen_zone, loyer_m2_moyen_zone)
     projection_df_cash_flow_pos = projection_df[projection_df['cash_flow_annuel']>=0]
     print(projection_df_cash_flow_pos)
 
@@ -200,3 +282,9 @@ if __name__=='__main__':
     print('Rapport de projection genere pour un bien à {} avec une superficie de {} m2'
           ' dans une zone ou le prix au m2 moyen est de {} euros et le loyer au m2 '
           'moyen est de {} euros'.format(ville, superficie_bien_m2, prix_m2_moyen_zone, loyer_m2_moyen_zone))
+
+    fiscalite_lmnp_df = pd.DataFrame(compta_par_an)
+    print('')
+    print('')
+    print(fiscalite_lmnp_df)
+    fiscalite_lmnp_df.to_csv('fiscalite_lmnp.csv')
